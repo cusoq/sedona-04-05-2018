4e29655457ea883c9f5575c10ab23fdfc6e93267
cusoq <cusoq@mail.ru>
2018-05-12 00:33:02 +0400

    Fixed menu script

--- js/script.js
+++ js/script.js
@@ -1,23 +1,19 @@
 var link = document.querySelector(".main-nav__toggle--open");
 var popup = document.querySelector(".main-nav__list");
 var close = popup.querySelector(".main-nav__toggle--close");
+var logomove = document.querySelector(".page-header__logo");
+var headermove = document.querySelector(".page-header");
 link.addEventListener("click", function (evt) {
 evt.preventDefault();
 popup.classList.add("popup--show");
 link.classList.add("visually-hidden");
+logomove.classList.remove("page-header__logo--moved");
+headermove.classList.remove("page-header--moved");
 });
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("popup--show");
   link.classList.remove("visually-hidden");
-});
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (popup.classList.contains("popup--show")) {
-      popup.classList.remove("popup--show");
-    }
-    if (link.classList.contains("visually-hidden")) {
-      link.classList.remove("visually-hidden");
-    }
-  }
+  logomove.classList.add("page-header__logo--moved");
+  headermove.classList.add("page-header--moved");
 });
